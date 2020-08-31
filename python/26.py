925628b53ba52176f6c876e108676180399f4895
Mike <notubw@gmail.com>
2020-08-31 02:28:23 -0500

    Now in Python

---
+++ python/26.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def removeDuplicates(self, nums):
+        i = 0
+        j = 0
+        for j in range(len(nums)):
+            if nums[j] != nums[i]:
+                i += 1
+                nums[i] = nums[j]
+        return i + 1
