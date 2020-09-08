d8fd23f822d4dd6ee6a462253f0e7145f8f5884e
Mike <notubw@gmail.com>
2020-09-07 22:53:39 -0500

    Leetcode 1480 Now in Java and Javascript

---
+++ javascript/1480.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var runningSum = function(nums) {
+    
+        for(var i=1;i<nums.length;i++){
+        nums[i]=nums[i-1]+nums[i];
+        }
+        return nums;
+        
+    
+};
