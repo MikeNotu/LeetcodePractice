4702978144cfd6bcf1dd6bc333a01b93d252ed5e
Mike <notubw@gmail.com>
2020-09-07 22:40:17 -0500

    easiest leetcode problem

---
+++ cs/1480.cs
@@ -0,0 +1,10 @@
+//Easiest leetcode problem honestly 
+public class Solution {
+    public int[] RunningSum(int[] nums) {
+        
+        for(int i=1;i<nums.Length;i++){
+            nums[i]=nums[i-1]+nums[i];
+        }
+        return nums;
+    }
+}
