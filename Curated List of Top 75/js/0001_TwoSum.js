e74e3862ac32ed4b9db31677f3f4a761ae9994dd
MikeNotu <mikebateman98@gmail.com>
2021-04-19 00:59:17 -0500

    added a semicolon

--- Curated List of Top 75/js/0001_TwoSum.js
+++ Curated List of Top 75/js/0001_TwoSum.js
@@ -9,7 +9,7 @@ var twoSum = function(nums, target) {
     for(let i = 0;i<nums.length;i++){
         if(map.has(nums[i])){
             return[map.get(nums[i]),i];
-        }
+        };
         let diff = target - nums[i];
         map.set(diff,i);
     };
