f779f00e32c5f4d05784ba59f8aff5f2b22d63e2
MikeNotu <mikebateman98@gmail.com>
2021-04-20 00:00:57 -0500

    New leetcode

---
+++ Curated List of Top 75/js/0121_BestTimeStock.js
@@ -0,0 +1,24 @@
+// You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+// You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+// Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+var maxProfit = function(prices) {
+    
+    let buy = prices[0];
+    prices[0] = 0;
+    let profit = 0;
+    for(let i = 1;i<prices.length;i++){
+        
+        if(buy>prices[i]){
+            buy = prices[i];
+            prices[i] = 0;
+        }else{
+            profit = Math.max(prices[i]-buy,profit)
+        }
+        
+    }
+    return profit;
+    
+};
