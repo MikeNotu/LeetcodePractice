12ec7773b628f23143e1ffb2e85ecdec0b78d702
MikeNotu <mikebateman98@gmail.com>
2021-04-23 14:15:22 -0500

    Added example to TwoSUm

--- Curated List of Top 75/js/0001_TwoSum.js
+++ Curated List of Top 75/js/0001_TwoSum.js
@@ -3,6 +3,11 @@
 // You may assume that each input would have exactly one solution, and you may not use the same element twice.
 
 // You can return the answer in any order.
+
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+// Output: Because nums[0] + nums[1] == 9, we return [0, 1].
+
 var twoSum = function(nums, target) {
     const map = new Map();
     
