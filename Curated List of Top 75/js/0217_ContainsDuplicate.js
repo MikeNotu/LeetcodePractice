b4d624377df6b4555fe2df72873062e3657877e3
MikeNotu <mikebateman98@gmail.com>
2021-04-25 15:10:04 -0500

    Added example

--- Curated List of Top 75/js/0217_ContainsDuplicate.js
+++ Curated List of Top 75/js/0217_ContainsDuplicate.js
@@ -1,5 +1,8 @@
 // Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
 
+// Input: nums = [1,2,3,1]
+// Output: true
+
 var containsDuplicate = function(nums) {
     
     let numbers = new Set();
