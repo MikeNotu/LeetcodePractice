32be5166d3c5afed41894c949e4b85df55fe4dc7
MikeNotu <mikebateman98@gmail.com>
2021-04-28 21:04:29 -0500

    Example 2 TwoSum

--- Curated List of Top 75/js/0001_TwoSum.js
+++ Curated List of Top 75/js/0001_TwoSum.js
@@ -8,6 +8,9 @@
 // Output: [0,1]
 // Output: Because nums[0] + nums[1] == 9, we return [0, 1].
 
+// Input: nums = [3,2,4], target = 6
+// Output: [1,2]
+
 var twoSum = function(nums, target) {
     const map = new Map();
     
