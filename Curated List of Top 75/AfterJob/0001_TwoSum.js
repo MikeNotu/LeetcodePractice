46a59623f9da5385b8c41179cdf338f28a0a161c
MikeNotu <mikebateman98@gmail.com>
2022-05-09 11:40:07 -0500

    Two Sum from memory

--- Curated List of Top 75/AfterJob/0001_TwoSum.js
+++ Curated List of Top 75/AfterJob/0001_TwoSum.js
@@ -3,19 +3,16 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
-    
-    for(let i = 0 ; i < nums.length-1 ; i++){
-        
-        const Needed = target-nums[i]
-        
-            for(let j = i+1 ; j < nums.length ; j++){
-        
-            if(nums[j]===Needed){
-                return [i,j]
-            }        
-    
+const twoSum = (nums, target) => {
+    const previousValues = {};
+
+    for (let i = 0; i < nums.length; i++) {
+        const neededValue = target - nums[i];
+
+        if (previousValues[neededValue] != null) {
+            return [previousValues[neededValue], i];
+        } else {
+            previousValues[nums[i]] = i;
+        }
     }
-    }
-    
 };
